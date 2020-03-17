dffd06deaaf108fa9e9f6f9fd445ecc81e0b0371
Xa Sherrod <xaviersherrod99@yahoo.com>
2020-03-17 05:01:41 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  num1 = 5
+  num2 = 4
+  sum =(num1 + num2)
 end
 
 def subtraction(num1, num2)
-
+  num1 = 10
+  num2 =5
+  subtraction = (num1 - num2)
 end
 
 def division(num1, num2)
-
+  num1 = 50
+  num2 = 2
+  qutient = (num1/num2)
 end
 
 def multiplication(num1, num2)
-
+  num1 = 4
+  num2 = 30 
+  product = (num1*num2)
 end
 
 def modulo(num1, num2)
-
+ num1 = 34
+ num2 = 5
+ qutient = (num1%num2)
 end
 
 def square_root(num)
-
+  num1 =81
+  num2 =9
+  banana = (num1/num2)
 end
