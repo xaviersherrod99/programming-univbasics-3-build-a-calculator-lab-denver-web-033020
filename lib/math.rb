af900df63ef212a5f5e69927ee3522f388f5aad8
Xa Sherrod <xaviersherrod99@yahoo.com>
2020-03-17 04:50:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,11 +5,15 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  num1 = 10
+  num2 =5
+  subtraction = (num1 - num2)
 end
 
 def division(num1, num2)
-
+  num1
+  num2
+  qu
 end
 
 def multiplication(num1, num2)
